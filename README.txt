b98a76b80818e09f01055782db2f76e9c12fb95f
stllglobal <emryazi@gmail.com>
2026-08-26 20:29:33 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+STLL Global static website package.
+Upload all files to the root of a GitHub repository.
+GitHub Pages: Settings > Pages > Deploy from a branch > main / root.
+Custom domain: www.stllglobal.com
---
+++ script.js
@@ -0,0 +1,1 @@
+const btn=document.querySelector('.menu');const nav=document.querySelector('.nav nav');btn.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('.nav nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
